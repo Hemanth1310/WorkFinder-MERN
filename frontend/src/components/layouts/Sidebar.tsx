3fa9792bbb9aa889ec86b74ed0dcf8ea3a102de4
Hemanth1310 <hemanth13k@gmail.com>
2026-04-08 15:05:57 +0200

    frontned-sidebar-filter-job-type

--- frontend/src/components/layouts/Sidebar.tsx
+++ frontend/src/components/layouts/Sidebar.tsx
@@ -5,16 +5,33 @@ const Sidebar = () => {
         <p className="font-semibold text-lg">Filter</p>
         <div className="w-full h-0.5 mt-5 mb-5 bg-mist-200"></div>
         <div className="mt-5 mb-5">
-        <p className="mb-3">Category</p>
-        <div className=" border border-mist-200   box-border text-sm rounded-md p-3">
-            <select className="w-full ">
-                <option>Sofware Development</option>
-                <option>Design</option>
-                <option>Sales</option>
-                <option>Marketing</option>
-                <option>Finances</option>
-            </select>
+            <p className="mb-3">Category</p>
+            <div className=" border border-mist-200   box-border text-sm rounded-md p-3">
+                <select className="w-full outline-none">
+                    <option>Sofware Development</option>
+                    <option>Design</option>
+                    <option>Sales</option>
+                    <option>Marketing</option>
+                    <option>Finances</option>
+                </select>
+            </div>
         </div>
+        <div className="mt-5 mb-5">
+            <p className="mb-3">Job Type</p>
+            <div className=" border border-mist-200   box-border text-sm rounded-md p-3">
+              <div className="flex gap-2 mb-2">
+                <input name='jobType' type='checkbox'></input>
+                <label className="text-xs">Full time</label>
+              </div>
+               <div className="flex gap-2 mb-2">
+                <input name='jobType' type='checkbox'></input>
+                <label className="text-xs">Intership</label>
+              </div>
+               <div className="flex gap-2 ">
+                <input name='jobType' type='checkbox'></input>
+                <label className="text-xs">Freelance</label>
+              </div>
+            </div>
         </div>
     </div>
   )
