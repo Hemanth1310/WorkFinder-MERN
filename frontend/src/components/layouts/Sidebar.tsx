b971d02576d1fbe3bb551a2fdf5d52ce0e0c85c5
Hemanth1310 <hemanth13k@gmail.com>
2026-04-08 14:47:15 +0200

    frontend-holy-layout

---
+++ frontend/src/components/layouts/Sidebar.tsx
@@ -0,0 +1,10 @@
+
+const Sidebar = () => {
+  return (
+    <div className="flex-1 min-h-full rounded-2xl p-5 box-border border border-mist-200 shadow-sm">
+        <p className="font-semibold text-lg">Filter</p>
+    </div>
+  )
+}
+
+export default Sidebar
