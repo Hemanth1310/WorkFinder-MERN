f3fc73794a99e320fcbd9038e8afef80ca05ea7a
Hemanth1310 <hemanth13k@gmail.com>
2026-04-09 17:20:44 +0200

    frontend-modal

--- frontend/src/components/layouts/Model.tsx
+++ frontend/src/components/layouts/Model.tsx
@@ -15,7 +15,7 @@ const Model = ({isOpen,onClose,title,children}: Props) => {
     
     const modalRoot  = document.getElementById('modal-root')
     const modalContent = (
-    <div className='fixed bg-gray-500/50 inset-0 flex items-center justify-center z-100 p-5' onClick={onClose}>
+    <div className='fixed bg-gray-500/50 inset-0 flex items-center justify-center z-100 p-5 font-mono' onClick={onClose}>
         <div className='bg-mist-50 p-5 rounded-md min-w-1/3 flex flex-col items-center gap-2' onClick={(e)=>e.stopPropagation()}>
             <div className='w-full flex items-center justify-between '>
                  <p className='text-2xl'>{title}</p>
