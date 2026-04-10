28877a0924e246159b6f83dc4439838ec06515ed
Hemanth1310 <hemanth13k@gmail.com>
2026-04-10 11:00:39 +0200

    frontend-login-working

--- frontend/src/components/layouts/Login.tsx
+++ frontend/src/components/layouts/Login.tsx
@@ -1,17 +1,26 @@
 import React, { useState } from 'react'
 import { loginDetailSchema } from '../../utils/typechecker'
+import axios from '../../utils/authMiddleware'
+import { AxiosError } from 'axios'
+import { toast } from 'react-toastify'
+import { useNavigate } from 'react-router'
+
+const BASE_API_URL = import.meta.env.VITE_API_URL
 
 type Props = {
-    handleToggle:(tab:string)=>void
+    handleToggle:(tab:string)=>void,
+    onClose:()=>void
 }
 
-const Login = ({handleToggle}: Props) => {
+const Login = ({handleToggle, onClose}: Props) => {
+    const navigate = useNavigate()
     const [errors, setErrors] = useState({
         email:"",
-        password:""
+        password:"",
+        apiResponse:""
     })
     
-    const handleForm =(formData: FormData) =>{
+    const handleForm =async(formData: FormData) =>{
         const logindetails =Object.fromEntries(formData.entries())
         const parsedLoginDetails = loginDetailSchema.safeParse(logindetails)
         if(!parsedLoginDetails.success){
@@ -21,9 +30,37 @@ const Login = ({handleToggle}: Props) => {
             }
         }
 
-        console.log(parsedLoginDetails.data)
+        try{
+           await axios.post(`${BASE_API_URL}/api/auth/login`,parsedLoginDetails.data)
+             toast.success('Login Successful',{
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: false,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                    })
+             onClose()   
+            navigate('/')
 
-
+        }catch(err){
+            if(err instanceof AxiosError){
+                setErrors(prev=>({...prev,apiResponse:err.response.data.error}))
+            }
+             setErrors(prev=>({...prev,apiResponse:"Unexpected Error Occured. Try again Later!"}))
+            toast.error('Login Failed',{
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: false,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                    })
+            }  
     }
   return (
     <form action={handleForm} className='w-full h-full flex flex-col items-center gap-5'>
@@ -37,9 +74,10 @@ const Login = ({handleToggle}: Props) => {
             <input name='password' type='password' className='border-2 rounded-lg border-mist-200 text-lg p-1' placeholder=''></input>
              {errors.password && <p className='text-xs text-red-500'>{errors.password}</p>}
         </div>
+        {errors.apiResponse && <p className='text-xs text-red-500'>Login Failed: {errors.apiResponse}</p>}
         <button className='w-full h-10 p-1 bg-brand-primary rounded-lg text-white'>Login</button>
         
-        <p>Not a user ? <span className='text-blue-500' onClick={()=>handleToggle('register')}>Register Here</span></p>
+        <p>Not registered ? <span className='text-blue-500' onClick={()=>handleToggle('register')}>Register Here</span></p>
         
     </form>
   )
