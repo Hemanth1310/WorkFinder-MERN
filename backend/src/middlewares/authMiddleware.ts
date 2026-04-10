93fb749abec8019546575dc7ba3f322d7118e1e5
Hemanth1310 <hemanth13k@gmail.com>
2026-04-10 12:08:07 +0200

    backend-middelware

---
+++ backend/src/middlewares/authMiddleware.ts
@@ -0,0 +1,36 @@
+import type { Request, Response,NextFunction } from "express";
+import jwt,{ JsonWebTokenError } from "jsonwebtoken";
+import type { decodedTokenPayload } from "../types.js";
+
+declare global {
+  namespace Express {
+    interface Request {
+      userData?: decodedTokenPayload;
+    }
+  }
+}
+
+const jwtSecret = process.env.JWT_TOKEN || '123456789'
+
+const authenticateToken = (
+    req:Request,
+    res:Response,
+    next:NextFunction
+)=>{
+    const token = req.cookies.token
+
+    if(!token){
+        return res.status(404).json({error:"Token not found."})
+    }
+
+    try{
+        const decoded = jwt.verify(token,jwtSecret) as decodedTokenPayload
+        req.userData = {...decoded}
+        next()
+    }catch(err){
+         return res.status(403).json({error:"Token expired."})
+    }
+   
+}
+
+export default authenticateToken
