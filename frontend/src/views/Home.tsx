f3d098126f8b47abbf800bb19ff6ec5a322c1fb8
Hemanth1310 <hemanth13k@gmail.com>
2026-04-14 13:36:14 +0200

    frontend-routing-home

--- frontend/src/views/Home.tsx
+++ frontend/src/views/Home.tsx
@@ -1,7 +1,26 @@
+import Loading from "../components/layouts/Loading"
+import { useAuthContextData } from "../utils/useAuthContextData"
+import EmployerDashboard from "./admin/EmployerDashboard"
+import CandidateDashboard from "./candidate/CandidateDashboard"
 
 const Home = () => {
+
+    const {userData} = useAuthContextData()
+
+    if(!userData){
+        return 
+    }
+
+    if(userData.role==='CANDIDATE'){
+        return <CandidateDashboard/>
+    }
+
+    if(userData.role==='EMPLOYER'){
+        return <EmployerDashboard/>
+    }
+    
   return (
-    <div>Home</div>
+    <Loading/>
   )
 }
 
