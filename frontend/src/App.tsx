c8f7b45628e08857e2493363f9c275bc0465a401
Hemanth1310 <hemanth13k@gmail.com>
2026-04-15 13:58:22 +0200

    frontend-css-fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
     <BrowserRouter>
     <div className='min-w-screen min-h-screen flex flex-col items-center bg-indigo-50 p-5 font-mono'>
         <Header/>
-        <div className="pl-5 pr-5 pb-5 flex-1 w-full flex gap-5 bg-mist-50 border border-mist-200 rounded-2xl p-5">
+        <div className="p-5 flex-1 w-full flex gap-5 bg-mist-50 border border-mist-200 rounded-2xl">
           <div className="flex-5 min-h-full rounded-2xl p-5 box-border">
             <Routes>
               <Route path="/" element={<Home/>}></Route>
