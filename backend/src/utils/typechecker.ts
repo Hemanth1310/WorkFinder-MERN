70df2335788de905e54ea108af07af145e4b0ca7
Hemanth1310 <hemanth13k@gmail.com>
2026-04-15 13:58:10 +0200

    backend-zod-job-post-schema

--- backend/src/utils/typechecker.ts
+++ backend/src/utils/typechecker.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod';
-import { Role } from '../../generated/prisma/enums.js';
+import { Role, JobType, Experience, Category } from '../../generated/prisma/enums.js';
 const emailRules = z.string().email("Invalid email format").trim().toLowerCase()
 
 // Define password rules ONCE so they are identical everywhere
@@ -10,6 +10,9 @@ const passwordRules = z.string()
 
 
 export const roleSchema = z.enum(Role)
+export const jobTypeSchema = z.enum(JobType)
+export const experienceSchema = z.enum(Experience)
+export const categorySchema = z.enum(Category)
 export type RoleType = z.infer<typeof roleSchema>
 
 export const loginDetailSchema = z.object({
@@ -24,3 +27,16 @@ export const registerSchema = z.object({
     password: passwordRules,
     role: roleSchema.default('CANDIDATE'),
 })
+
+export const jobPostingScema = z.object({
+    title: z.string().min(1,"Title cannot be empty"),
+    description: z.string().min(1,"Description cannot be empty"),
+    companyName: z.string().min(1,"Company Name cannot be empty"),
+    location: z.string().min(1,"Location Cannot be empty"),
+    salary: z.number().int().nonnegative().nullable().optional().default(null),
+    jobType: jobTypeSchema,
+    experience: experienceSchema,
+    category: categorySchema
+})
+
+
