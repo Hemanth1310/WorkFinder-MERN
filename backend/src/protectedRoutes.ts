e4189d9c8a99d3f086750628d65c5d34b24a8107
Hemanth1310 <hemanth13k@gmail.com>
2026-04-15 13:57:43 +0200

    backend-zod-jobpost-schema

--- backend/src/protectedRoutes.ts
+++ backend/src/protectedRoutes.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import { prisma } from './prisma.js'
-
+import type { JobPostingCreateInput } from '../generated/prisma/models.js'
+import { jobPostingScema } from './utils/typechecker.js'
+import { PrismaClientKnownRequestError } from '../generated/prisma/internal/prismaNamespace.js'
 const router = express.Router()
 
 
@@ -43,6 +45,49 @@ router.post('/user-details',async(req,res)=>{
     }catch(error){
          return res.status(404).json({error:"User not found."})
     }
+})
+
+router.post('/make-a-post',async(req,res)=>{
+   const userPayload =  req.userData 
+   const jobPosting = req.body
+   const parsedJobPosting =  jobPostingScema.safeParse(jobPosting)
+
+   if(!parsedJobPosting.success){
+    return res.status(400).json({error:"Invalid job details"})
+   }
+
+   if(!userPayload){
+    return res.status(400).json({error:"Invalid User"})
+   }
+
+   try{
+    const user  = await prisma.user.findFirst({
+        where:{
+            id:userPayload?.id
+        }
+    })
+
+    if(!user){
+        return res.status(400).json({error:"Invalid User"})
+    }
+    await prisma.jobPosting.create({
+        data:{
+            ...parsedJobPosting.data,
+            employerId:user.id
+        }
+    })
+    return res.status(201).json({message:"Job posted"})
+   }catch(error){
+        if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === "2002") {
+            return res
+            .status(401)
+            .json({ errorMessage: "Unable to porcess request." });
+        }
+        }
+        console.error("Unexpected Error:", error);
+        return res.status(500).json({ errorMessage: "Internal server error" });
+   }
 })
 
 export default router
